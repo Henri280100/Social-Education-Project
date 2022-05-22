3daa8725150bb6ba89df67d32d280e58d6c7d844
Henri280100 <69888512+Henri280100@users.noreply.github.com>
2022-05-22 22:07:12 +0700

    Update index.router.js

--- BackEnd/Routers/index.router.js
+++ BackEnd/Routers/index.router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const userController = require('../app/controllers/UserController');
+const userController = require('../Controller/userController');
 
 router.post('/register', userController.register);
 router.post('/delete', userController.delete);
